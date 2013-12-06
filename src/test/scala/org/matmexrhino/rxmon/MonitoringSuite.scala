060e5796a9f74e4e14c8a8ea85f7482b2c53c44c
vigdorchik <eugene.vigdorchik@gmail.com>
2013-12-06 14:00:28 +0400

    More tests.

--- src/test/scala/org/matmexrhino/rxmon/MonitoringSuite.scala
+++ src/test/scala/org/matmexrhino/rxmon/MonitoringSuite.scala
@@ -5,8 +5,8 @@ import org.scalatest.junit.JUnitRunner
 import org.junit.Assert._
 import org.scalatest.FunSuite
 import rx.lang.scala.Observable
-import rx.lang.scala.subscriptions.BooleanSubscription
 import Monitoring._
+import scala.concurrent.duration._
 
 @RunWith(classOf[JUnitRunner])
 class MonitoringSuite extends FunSuite {
@@ -19,13 +19,32 @@ class MonitoringSuite extends FunSuite {
 
   test("binop") {
     val range = 1 to 10
-    val X, Y: Observable[Int] = Observable { observer =>
-      for (i <- range) observer.onNext(i)
-      observer.onCompleted()
-      BooleanSubscription()
-    }
+    val X, Y: Observable[Int] = Observable(range)
     val F: Observable[Int] = X * Y
     // Range is propagated (deterministically) for one multiplier only. Why?
     assertEquals(range map (_ * range.last), F.toBlockingObservable.toList)
+  }
+
+  def cut[T](obs: Observable[T]): Observable[T] = obs.takeUntil(Observable.interval(1.seconds))
+
+  test("stable") {
+    val X: Observable[Double] = Observable.interval(50.milliseconds) map (x => math.sin(x.toDouble))
+    val F: Observable[Boolean] = cut((X < 1).stable(200.milliseconds))
+    assertTrue(F.toBlockingObservable.toList.forall { b => b })
+  }
+
+  test("monotonic") {
+    val X: Observable[Long] = Observable.interval(50.milliseconds)
+    val F: Observable[Double] = cut(X.avg(200.milliseconds))
+    val G: Observable[Long] = cut(X.min(200.milliseconds))
+    val H: Observable[Long] = cut(X.max(200.milliseconds))
+
+    def check[T: Ordering](obs: Observable[T]) {
+      val l = obs.toBlockingObservable.toList
+      assertTrue(l == l.sorted)
+    }
+    check(F)
+    check(G)
+    check(H)
   }
 }
