124a7a2ff460a8a4a9ef4acbc931f1089502f4c3
Eugene Vigdorchik <eugene.vigdorchik@gmail.com>
2014-02-25 20:31:32 +0400

    Convert to multi-project format.

---
+++ project/Build.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2013-2014 Eugene Vigdorchik.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
+
+import sbt._
+import sbt.Keys._
+
+object RxmonBuild extends Build {
+  lazy val commonSettings = Defaults.defaultSettings ++ Seq (
+    organization := "org.matmexrhino",
+    version := "0.2.0",
+    scalaVersion := "2.10.3",
+    resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+    libraryDependencies ++= Seq(
+	"org.scalatest" %% "scalatest" % "1.9.1" % "test",
+	"junit" % "junit" % "4.10" % "test"
+    ),
+    parallelExecution in Test := false
+  )
+
+  lazy val rxmon = Project (
+    id = "rxmon",
+    base = file("."),
+    aggregate = Seq(core)
+  )
+
+  lazy val core = Project (
+    id = "core",
+    base = file("core"),
+    settings = commonSettings ++ Seq (
+      name := "rxmon-core",
+      libraryDependencies ++= Seq(
+	"com.netflix.rxjava" % "rxjava-scala" % "0.16.0",
+	"com.typesafe.akka" %% "akka-actor" % "2.2.3",
+	"com.typesafe.akka" %% "akka-testkit" % "2.2.3" % "test"
+      )
+    )
+  )
+}
