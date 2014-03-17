3683f01f7d11a4624bfbcaf16e9c67f7a8d4145a
Eugene Vigdorchik <eugene.vigdorchik@gmail.com>
2014-03-17 12:34:23 +0400

    Depend on rxjava 0.17.1

--- project/Build.scala
+++ project/Build.scala
@@ -48,7 +48,7 @@ object RxmonBuild extends Build {
     settings = commonSettings ++ bintraySettings ++ Seq (
       name := "rxmon",
       libraryDependencies ++= Seq(
-	"com.netflix.rxjava" % "rxjava-scala" % "0.17.0",
+	"com.netflix.rxjava" % "rxjava-scala" % "0.17.1",
 	"com.typesafe.akka" %% "akka-actor" % "2.3.0",
 	"com.typesafe.akka" %% "akka-testkit" % "2.3.0" % "test"
       ),
